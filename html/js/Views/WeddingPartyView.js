cbef8ab2a691e537a91050e36c65b40590a54028
anton <anton@freshbooks.com>
2014-03-09 17:02:26 -0400

    Redid the site as a backbone app

---
+++ html/js/Views/WeddingPartyView.js
@@ -0,0 +1,36 @@
+var WeddingPartyView = ContentView.extend({
+
+  /*
+   * Initialize with the template-id
+   */
+  initialize: function(options) {
+    this.template = options.template;
+  },
+
+  render: function() {
+    var content = $(this.template).html();
+    $(this.el).html(content);
+
+    $('.view-bio').overlay({
+      top: 'center',
+      left: 'center',
+      mask: {
+        color: '#FFF',
+        loadSpeed: 200,
+        opacity: 0.9
+      },
+      closeOnClick: false
+    });
+    return this;
+  },
+
+  events: {
+    'click #view-bio': 'displayBio',
+    'click': 'displayBio'
+  },
+
+  displayBio: function() {
+    console.log('hello');
+  }
+
+});
